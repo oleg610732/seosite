534e70750cba83625a2023ec4d3920fa2b83f839
Anonymous <anon@example.com>
2026-07-29 13:46:33 +0300

    Updates

--- _astro/HeroCanvas.astro_astro_type_script_index_0_lang.BmKgINF_.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/hero-canvas.D1Cazj7C.js","_astro/canvas-utils.BAU0pBHR.js"])))=>i.map(i=>d[i]);
-import{b as a,_ as o}from"./boot-canvas-idle.L3sDQRwl.js";a(()=>o(()=>import("./hero-canvas.D1Cazj7C.js"),__vite__mapDeps([0,1])),{init:"initHeroCanvas",teardown:"teardownHeroCanvas"});
--- _astro/ServicesCanvas.astro_astro_type_script_index_0_lang.lC9kpfqd.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/services-canvas.CBgez7qf.js","_astro/canvas-utils.BAU0pBHR.js"])))=>i.map(i=>d[i]);
-import{b as a,_ as e}from"./boot-canvas-idle.L3sDQRwl.js";a(()=>e(()=>import("./services-canvas.CBgez7qf.js"),__vite__mapDeps([0,1])),{init:"initServicesCanvas",teardown:"teardownServicesCanvas"});
