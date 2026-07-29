95f78b984ed9ba973e588ccf509f77cad28ef500
Anonymous <anon@example.com>
2026-07-29 12:51:55 +0300

    Updates

---
+++ _astro/HeaderCanvas.astro_astro_type_script_index_0_lang.C73lB9j-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/header-canvas.BvnpTyoV.js","_astro/canvas-utils.BAU0pBHR.js"])))=>i.map(i=>d[i]);
+import{b as a,_ as e}from"./boot-canvas-idle.L3sDQRwl.js";a(()=>e(()=>import("./header-canvas.BvnpTyoV.js"),__vite__mapDeps([0,1])),{init:"initHeaderCanvas",teardown:"teardownHeaderCanvas"});
---
+++ _astro/HeroCanvas.astro_astro_type_script_index_0_lang.BP5j9vzX.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/hero-canvas.DT_SmXkz.js","_astro/canvas-utils.BAU0pBHR.js"])))=>i.map(i=>d[i]);
+import{b as a,_ as o}from"./boot-canvas-idle.L3sDQRwl.js";a(()=>o(()=>import("./hero-canvas.DT_SmXkz.js"),__vite__mapDeps([0,1])),{init:"initHeroCanvas",teardown:"teardownHeroCanvas"});
